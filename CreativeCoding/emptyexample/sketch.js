c640acd94c7d536952f45d49219f644ebceb55c7
labthings4me <zoehedstrom@yahoo.com>
2024-01-07 17:58:04 -0500

    removeemptyexmple

--- CreativeCoding/emptyexample/sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() { //truns one timne
-  
-}
-
-function draw() { //runs in a loop
- 
-}
--- CreativeCoding/emptyexamplecopy/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() { //truns one timne
-  createCanvas {windowWidth,windowHeight}; // canvas sized to wondow
-  background {255,0,0}; 
-}
-
-function draw() { //runs in a loop
- 
-}
