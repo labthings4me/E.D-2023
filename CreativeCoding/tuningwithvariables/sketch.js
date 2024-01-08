fd90d4b28aab1dfa93aa0ff454afa18ccf845afd
labthings4me <zoehedstrom@yahoo.com>
2024-01-08 14:23:59 -0500

    update variable

--- CreativeCoding/tuningwithvariables/sketch.js
+++ CreativeCoding/tuningwithvariables/sketch.js
@@ -1,6 +1,53 @@
-function setup() { // rusn one time
+//create programme to customise number of sqaures in a grid
+// two variables 1. # of sqaures
+                //2. side lengthe based on wondow width
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  fill('pink')
+  stroke ('black');
+  background (150)
+  strokeWeight (1);
+  
+}
+
+function draw() {
+  
+  
+  var num  = 2; // var for # of sqaures
+  var sideLen = windowWidth/num ; // var for sid Len ea sqaure
+  
  
 
-function draw() { // runs in a loop
+  
+
+  
+  for (var y = 0; y < windowHeight; y = y + sideLen) {   //loop for y direction
+    
+     for (var x = 0; x < windowWidth; x = x + sideLen )  { //loop x direction
+  
+  quad (x,y,
+        x + sideLen,y,
+        x + sideLen, y + sideLen,
+        x,y + sideLen);
+    
+  }
+    
+    
+    
+    
+    
+  }
+  
+ 
+  
+
+  
+   
+ 
+  
  
 }
+function  windowResized () {
+resizeCanvas (windowWidth, windowHeight);
